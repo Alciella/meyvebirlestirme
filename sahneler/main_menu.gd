972afc39a3d061e7c24d4fe505a264ca47a5b5bc
asenesyova <ilyaskaplan1108@gmail.com>
2025-05-09 21:00:50 +0300

    ses paneli

--- sahneler/main_menu.gd
+++ sahneler/main_menu.gd
@@ -19,3 +19,7 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().quit() # Replace with function body.
+
+
+func _on_ayarlar_pressed() -> void:
+	get_tree().change_scene_to_file("res://ses_ayarları.tscn")
